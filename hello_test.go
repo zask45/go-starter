ae8799acd4e2b93c465b55c3a9749ca1c946ba87
zask45 <zaschakeysha@gmail.com>
2024-12-27 18:33:47 +0700

    add test for another language

--- hello_test.go
+++ hello_test.go
@@ -3,21 +3,38 @@ package main
 import "testing"
 
 func TestGreeting(t *testing.T) {
-	t.Run("saying hello to other people", func(t *testing.T) {
-		got := Greeting("Yuta-kun")
+	t.Run("greet user in japanese", func(t *testing.T) {
+		got := Greeting("Yuta-kun", "")
 		want := "Konnichiwa Yuta-kun"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("greet user in spanish", func(t *testing.T) {
+		got := Greeting("Yuta-kun", "spanish")
+		want := "Hola Yuta-kun"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("greet user in french", func(t *testing.T) {
+		got := Greeting("Yuta-kun", "french")
+		want := "Bonjour Yuta-kun"
+
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Konnichiwa' when an empty string is supplied", func(t *testing.T) {
-		got := Greeting("")
+		got := Greeting("", "")
 		want := "Konnichiwa"
 
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
