07f94c36b5dc8190225b1605c8840fd451a7ef78
zask45 <zaschakeysha@gmail.com>
2024-12-27 16:56:10 +0700

    add another test

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestGreeting(t *testing.T) {
-	got := Greeting()
-	want := "Konnichiwa"
+	t.Run("saying hello to other people", func(t *testing.T) {
+		got := Greeting("Yuta-kun")
+		want := "Konnichiwa Yuta-kun"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+
+	t.Run("say 'Konnichiwa' when an empty string is supplied", func(t *testing.T) {
+		got := Greeting("")
+		want := "Konnichiwa"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
