feff1734e0acb20b12e96b2c5e95611b332efe83
zask45 <zaschakeysha@gmail.com>
2024-12-27 18:20:42 +0700

    add another language

--- hello.go
+++ hello.go
@@ -4,16 +4,32 @@ import (
 	"fmt"
 )
 
-const helloInJapanese = "Konnichiwa"
+const (
+	spanish = "spanish"
+	french  = "french"
 
-func Greeting(name string) string {
+	hello          = "Konnichiwa"
+	helloInSpanish = "Hola"
+	helloInFrench  = "Bonjour"
+)
+
+func Greeting(name string, language string) string {
 	if name == "" {
-		return helloInJapanese
+		return hello
 	}
 
-	return helloInJapanese + " " + name
+	switch language {
+	case spanish:
+		return helloInSpanish + " " + name
+	case french:
+		return helloInFrench + " " + name
+	}
+
+	return hello + " " + name
 }
 
 func main() {
-	fmt.Println(Greeting("Yuta-kun"))
+	fmt.Println(Greeting("Yuta-kun", ""))
+	fmt.Println(Greeting("Yuta-kun", spanish))
+	fmt.Println(Greeting("Yuta-kun", french))
 }
