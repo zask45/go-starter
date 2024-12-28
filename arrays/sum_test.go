6e944ca9c9f1ad76ad63ff05f6e204cffa5ee230
zask45 <zaschakeysha@gmail.com>
2024-12-28 17:43:42 +0700

    refactor first test

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := [5]int{1, 2, 3, 4, 5}
+		numbers := []int{1, 2, 3, 4, 5}
 
 		got := Sum(numbers)
 		expected := 15
