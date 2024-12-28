19f9db4d53180dd793a17affcf92226a4d8ee8c2
zask45 <zaschakeysha@gmail.com>
2024-12-28 17:27:48 +0700

    add test for slices

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -3,12 +3,25 @@ package arrays
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	expected := 15
+		got := Sum(numbers)
+		expected := 15
 
-	if got != expected {
-		t.Errorf("got %d expected %d given %v", got, expected, numbers)
-	}
+		if got != expected {
+			t.Errorf("got %d expected %d given %v", got, expected, numbers)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		expected := 6
+
+		if got != expected {
+			t.Errorf("got %d expected %d given %v", got, expected, numbers)
+		}
+	})
 }
