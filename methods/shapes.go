1716d1925af04dc21206417ccfc66c68b8917859
zask45 <zaschakeysha@gmail.com>
2024-12-29 20:29:53 +0700

    change parameter

--- methods/shapes.go
+++ methods/shapes.go
@@ -5,10 +5,10 @@ type Rectangle struct {
 	Height float64
 }
 
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
 }
 
-func Area(width float64, height float64) float64 {
-	return width * height
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
 }
