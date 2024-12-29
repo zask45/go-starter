e19f8461942bc26de05a8123ca39e9ae0e221d24
zask45 <zaschakeysha@gmail.com>
2024-12-29 20:55:02 +0700

    add circle struct

--- methods/shapes.go
+++ methods/shapes.go
@@ -5,6 +5,10 @@ type Rectangle struct {
 	Height float64
 }
 
+type Circle struct {
+	Radius float64
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
