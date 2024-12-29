80483f6d3c2ab1ba41a9ee7673fd2c6c247ae282
zask45 <zaschakeysha@gmail.com>
2024-12-29 17:22:33 +0700

    change file names

--- methods/area.go
+++
@@ -1,9 +0,0 @@
-package area
-
-func Perimeter(width float64, length float64) float64 {
-	return 2 * (width + length)
-}
-
-func Area(width float64, length float64) float64 {
-	return width * length
-}
---
+++ methods/shapes.go
@@ -0,0 +1,14 @@
+package area
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func Perimeter(width float64, height float64) float64 {
+	return 2 * (width + height)
+}
+
+func Area(width float64, height float64) float64 {
+	return width * height
+}
