efa39fe2f58e7c9a29d5986994432a76040249c7
zask45 <zaschakeysha@gmail.com>
2024-12-29 21:02:18 +0700

    write minimal method for area

--- methods/shapes.go
+++ methods/shapes.go
@@ -5,8 +5,16 @@ type Rectangle struct {
 	Height float64
 }
 
+func (r Rectangle) Area() float64 {
+	return 0
+}
+
 type Circle struct {
 	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return 0
 }
 
 func Perimeter(rectangle Rectangle) float64 {
