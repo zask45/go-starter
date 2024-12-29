ca1fc4208f1906b2247542d7a18b93edbcae849b
zask45 <zaschakeysha@gmail.com>
2024-12-30 01:42:01 +0700

    refactor test

--- methods/shapes_test.go
+++ methods/shapes_test.go
@@ -13,23 +13,26 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		got := rectangle.Area()
-		want := 72.0
 
+	checkSum := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %.2f want %.2f", got, want)
 		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		want := 72.0
+
+		checkSum(t, rectangle, want)
 	})
 
 	t.Run("circle", func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area()
 		want := 314.1592653589793
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
+		checkSum(t, circle, want)
 	})
 }
