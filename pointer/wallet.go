8b7e9e14ca6230cc8fd5dabb5e24fc775fa063dd
zask45 <zaschakeysha@gmail.com>
2024-12-30 17:09:21 +0700

    add Deposit and Balance methods

--- pointer/wallet.go
+++ pointer/wallet.go
@@ -2,3 +2,11 @@ package wallet
 
 type Wallet struct {
 }
+
+func (w Wallet) Deposit(amount int) {
+
+}
+
+func (w Wallet) Balance() int {
+	return 0
+}
--- pointer/wallet_test.go
+++ pointer/wallet_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
-	wallet.deposit(10)
+	wallet.Deposit(10)
 
 	got := wallet.Balance()
 	want := 10
