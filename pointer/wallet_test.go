7a5fb10b75379012cbdbb7f8ed61a80df634697b
zask45 <zaschakeysha@gmail.com>
2024-12-30 18:17:10 +0700

    add Withdraw bitcoin test

--- pointer/wallet_test.go
+++ pointer/wallet_test.go
@@ -3,13 +3,27 @@ package wallet
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
 
-	got := wallet.Balance().String()
-	want := "10 BTC"
+		got := wallet.Balance().String()
+		want := "10 BTC"
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Withdraw(Bitcoin(5))
+
+		got := wallet.Balance().String()
+		want := "5 BTC"
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
