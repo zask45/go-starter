9e678886efa07aeeabf54ff57023db2b8b86ede4
zask45 <zaschakeysha@gmail.com>
2024-12-30 17:12:36 +0700

    add balance field

--- pointer/wallet.go
+++ pointer/wallet.go
@@ -1,12 +1,13 @@
 package wallet
 
 type Wallet struct {
+	balance int
 }
 
 func (w Wallet) Deposit(amount int) {
-
+	w.balance += amount
 }
 
 func (w Wallet) Balance() int {
-	return 0
+	return w.balance
 }
