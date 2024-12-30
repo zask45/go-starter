47e7744072f784d1206001d9875e7b638c013ed2
zask45 <zaschakeysha@gmail.com>
2024-12-30 17:45:18 +0700

    refactor test

--- pointer/wallet_test.go
+++ pointer/wallet_test.go
@@ -4,10 +4,10 @@ import "testing"
 
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 
 	got := wallet.Balance()
-	want := 10
+	want := Bitcoin(10)
 
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
