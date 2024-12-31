bc1a21efb3e55c8010950cd2d35e4346264d252b
zask45 <zaschakeysha@gmail.com>
2024-12-31 15:53:23 +0700

    write AddTest

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -23,6 +23,20 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
+	want := "this is just a test"
+	got, err := dictionary.Search("test")
+
+	if err != nil {
+		t.Fatal("should find added word")
+	}
+
+	assertStrings(t, got, want)
+}
+
 func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
