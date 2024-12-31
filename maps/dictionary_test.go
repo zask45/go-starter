f2083d528cd010c21e0b556e730c33907da87f4a
zask45 <zaschakeysha@gmail.com>
2024-12-31 15:17:02 +0700

    revise known word test

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -6,7 +6,7 @@ func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
 	t.Run("known word", func(t *testing.T) {
-		got, _ := dictionary.Search(dictionary, "test")
+		got, _ := dictionary.Search("test")
 		want := "this is just a test"
 
 		assertStrings(t, got, want)
