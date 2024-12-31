f8e0c9a2e8e255914b0e0c3aeda2646f4246bd3d
zask45 <zaschakeysha@gmail.com>
2024-12-31 22:16:00 +0700

    fix Update to pass test

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -38,5 +38,6 @@ func (d Dictionary) Add(word, definition string) error {
 	return nil
 }
 
-func (d Dictionary) Update(word, definition string) {
+func (d Dictionary) Update(word, newDefinition string) {
+	d[word] = newDefinition
 }
