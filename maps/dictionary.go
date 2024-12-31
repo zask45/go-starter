c21301a9940c185e8a1a6a4902cdc919c4ec7908
zask45 <zaschakeysha@gmail.com>
2025-01-01 00:46:13 +0700

    fix Update to pass test

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -40,6 +40,16 @@ func (d Dictionary) Add(word, definition string) error {
 }
 
 func (d Dictionary) Update(word, newDefinition string) error {
-	d[word] = newDefinition
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		return ErrWordDoesNotExists
+	case nil:
+		d[word] = newDefinition
+	default:
+		return err
+	}
+
 	return nil
 }
