2a03c4f6323bdd13f4117618ca697bfabac1e2fb
zask45 <zaschakeysha@gmail.com>
2024-12-31 15:37:09 +0700

    refactor error

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -13,19 +13,26 @@ func TestSearch(t *testing.T) {
 	})
 
 	t.Run("unknown word", func(t *testing.T) {
-		_, err := dictionary.Search("unknown")
-		want := "could not find the word"
+		_, got := dictionary.Search("unknown")
 
-		if err == nil {
+		if got == nil {
 			t.Fatal("expected to get an error")
 		}
 
-		assertStrings(t, err.Error(), want)
+		assertError(t, got, ErrNotFound)
 	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q given %q", got, want, "test")
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
 	}
 }
