2701482be250d9fcc9c1b1892c2dc5356a17b3a6
zask45 <zaschakeysha@gmail.com>
2025-01-02 17:02:56 +0700

    add time.Sleep to Countdown

--- mocking/mock.go
+++ mocking/mock.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const finalWord = "Go!"
@@ -12,6 +13,7 @@ const countdownStart = 3
 func Countdown(writer io.Writer) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(writer, i)
+		time.Sleep(1 * time.Second)
 	}
 	fmt.Fprintf(writer, finalWord)
 }
