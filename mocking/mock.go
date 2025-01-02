63a11111854c00cfea43420838d8edda6140ee84
zask45 <zaschakeysha@gmail.com>
2025-01-02 21:53:34 +0700

    create real implementation of Countdown Sleeper

--- mocking/mock.go
+++ mocking/mock.go
@@ -19,6 +19,12 @@ func (s *SpySleeper) Sleep() {
 	s.Calls++
 }
 
+type DefaultSleeper struct{}
+
+func (d DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
 const finalWord = "Go!"
 const countdownStart = 3
 
@@ -31,5 +37,6 @@ func Countdown(writer io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
