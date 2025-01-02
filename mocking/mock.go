c246e334cf93036a97cb364d3477592fb80480fe
zask45 <zaschakeysha@gmail.com>
2025-01-02 21:47:30 +0700

    inject Sleeper as Countdown param

--- mocking/mock.go
+++ mocking/mock.go
@@ -22,7 +22,7 @@ func (s *SpySleeper) Sleep() {
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(writer io.Writer) {
+func Countdown(writer io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(writer, i)
 		time.Sleep(1 * time.Second)
