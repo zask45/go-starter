3674efe7a2df948721ae38255b1304e049881045
zask45 <zaschakeysha@gmail.com>
2025-01-03 18:16:38 +0700

    add SpyCountdownOperations

--- mocking/mock.go
+++ mocking/mock.go
@@ -25,6 +25,22 @@ func (d DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
 
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
 const finalWord = "Go!"
 const countdownStart = 3
 
