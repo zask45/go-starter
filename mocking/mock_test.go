0611ec3b9685250b9bc4218756c65ab7e5af23be
zask45 <zaschakeysha@gmail.com>
2025-01-03 18:08:34 +0700

    add sleep before print test

--- mocking/mock_test.go
+++ mocking/mock_test.go
@@ -2,26 +2,44 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
+	t.Run("prints 3 to Go!", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		Countdown(buffer, &SpyCountdownOperations{})
 
-	Countdown(buffer, spySleeper)
+		got := buffer.String()
+		want := `3
+	2
+	1
+	Go!`
 
-	got := buffer.String()
-	want := `3
-2
-1
-Go!`
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		}
+	})
 
-	if spySleeper.Calls != 3 {
-		t.Errorf("not enough calls to sleeper, want 3 instead of %d", spySleeper.Calls)
-	}
+	t.Run("sleep before every print", func(t *testing.T) {
+		spySleepPrinter := &SpyCountdownOperations{}
+		Countdown(spySleepPrinter, spySleepPrinter)
+
+		want := []string{
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+			sleep,
+			write,
+		}
+
+		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
+			t.Errorf("wanted calls %v got %v", want, spySleepPrinter)
+		}
+	})
+
 }
