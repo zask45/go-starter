12e803922ef372b704d05a2a51c8a33ca3478b41
zask45 <zaschakeysha@gmail.com>
2025-01-04 22:35:44 +0700

    add BenchmarkCheckWebsite

--- concurrency/check_website_benchmark.go
+++ concurrency/check_website_benchmark.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"testing"
 	"time"
 )
 
 func slowStubWebsiteChecker(_ string) bool {
 	time.Sleep(20 * time.Millisecond)
 	return true
+}
+
+func BenchmarkCheckWebsite(b *testing.B) {
+	urls := make([]string, 100)
+
+	for i := 0; i < len(urls); i++ {
+		urls[i] = "a url"
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		CheckWebsites(slowStubWebsiteChecker, urls)
+	}
 }
