982fa2c843f0d2bb07d93bb3c468226da6268572
zask45 <zaschakeysha@gmail.com>
2025-01-06 18:52:07 +0700

    add 2 secs sleep

--- concurrency/check_website.go
+++ concurrency/check_website.go
@@ -1,13 +1,19 @@
 package main
 
+import "time"
+
 type WebsiteChecker func(url string) bool
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func() {
+			results[url] = wc(url)
+		}()
 	}
+
+	time.Sleep(2 * time.Second)
 
 	return results
 }
