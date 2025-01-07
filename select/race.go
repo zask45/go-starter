fde2acf8a3e91a76e94927c5e0e92d1667dd8055
zask45 <zaschakeysha@gmail.com>
2025-01-07 17:07:59 +0700

    refactor Racer

--- select/race.go
+++ select/race.go
@@ -5,18 +5,22 @@ import (
 	"time"
 )
 
-func Racer(a, b string) string {
-	startA := time.Now()
-	http.Get(a)
-	aDuration := time.Since(startA)
-
-	startB := time.Now()
-	http.Get(b)
-	bDuration := time.Since(startB)
+func Racer(a, b string) (winner string) {
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
 
 	if aDuration < bDuration {
 		return a
 	}
 
 	return b
+}
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+
+	duration := time.Since(start)
+
+	return duration
 }
