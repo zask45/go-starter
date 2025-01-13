625efe9fff6834ba2c724386defe684f8dc6230f
zask45 <zaschakeysha@gmail.com>
2025-01-13 15:28:30 +0700

    fix walk to pass test

--- reflection/walk.go
+++ reflection/walk.go
@@ -6,6 +6,10 @@ func walk(x interface{}, fn func(input string)) {
 	val := reflect.ValueOf(x)
 
 	for i := 0; i < val.NumField(); i++ {
-		fn(val.Field(i).String())
+		field := val.Field(i)
+
+		if field.Kind() == reflect.String {
+			fn(field.String())
+		}
 	}
 }
